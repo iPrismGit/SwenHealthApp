2c25710a2b65a7b02bb91a241ef9813b2974b3e1
CHARAN TEJA <charan.teja@improve10x.com>
2026-03-27 16:40:28 +0530

    Designed Online Doctor Booking Patient Details And Summery Activity.

---
+++ app/src/main/java/com/iprism/swenhealth/activities/AddOnlineDoctorPatientDetailsActivity.kt
@@ -0,0 +1,60 @@
+package com.iprism.swenhealth.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.iprism.swenhealth.R
+import com.iprism.swenhealth.adapters.MultiFamilyMembersAdapter
+import com.iprism.swenhealth.databinding.ActivityAddOnlineDoctorPatientDetailsBinding
+import com.iprism.swenhealth.interfaces.OnMultipleFamilyItemClickListener
+
+class AddOnlineDoctorPatientDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddOnlineDoctorPatientDetailsBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAddOnlineDoctorPatientDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        handleBack()
+        handleConfirmBtn()
+        setUpFamilyMembers()
+    }
+
+    private fun handleConfirmBtn() {
+        binding.continueBookingBtn.setOnClickListener { p0 ->
+            startActivity(Intent(this, DiagnosticSummeryActivity::class.java))
+        }
+    }
+
+    private fun setUpFamilyMembers() {
+        val familyMembersAdapter = MultiFamilyMembersAdapter()
+        binding.familyMembersRv.layoutManager = GridLayoutManager(this, 3)
+        binding.familyMembersRv.adapter = familyMembersAdapter
+        familyMembersAdapter.setOnMultipleFamilyItemClickListener(object :
+            OnMultipleFamilyItemClickListener {
+            override fun onItemClicked(position: String) {
+                Log.d("Position", position)
+            }
+        })
+    }
+
+    private fun handleBack() {
+        binding.backImg.setOnClickListener { p0 ->
+            finish()
+        }
+    }
+
+}
--- app/src/main/java/com/iprism/swenhealth/activities/OnlineDoctorBookingSummeryActivity.kt
+++ app/src/main/java/com/iprism/swenhealth/activities/OnlineDoctorBookingSummeryActivity.kt
@@ -1,43 +1,27 @@
 package com.iprism.swenhealth.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.iprism.swenhealth.R
+import com.iprism.swenhealth.databinding.ActivityOnlineDoctorBookingSummeryBinding
 import com.iprism.swenhealth.databinding.ActivityOnlineDoctorProfileBinding
-import com.iprism.swenhealth.databinding.ActivityOnlineDoctorsBinding
 
-class OnlineDoctorProfileActivity : AppCompatActivity() {
+class OnlineDoctorBookingSummeryActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityOnlineDoctorProfileBinding
+    private lateinit var binding: ActivityOnlineDoctorBookingSummeryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityOnlineDoctorProfileBinding.inflate(layoutInflater)
+        binding = ActivityOnlineDoctorBookingSummeryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        handleBack()
-        handleContinueBtn()
     }
-
-    private fun handleContinueBtn() {
-        binding.onlineConsultBtn.setOnClickListener { p0 ->
-            startActivity(Intent(this, OnlineDoctorTimeSlotsActivity::class.java))
-        }
-    }
-
-    private fun handleBack() {
-        binding.backImg.setOnClickListener { p0 ->
-            finish()
-        }
-    }
-
 }
