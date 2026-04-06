372acdd4c20acfc65fadc0339da0c9cc400d451d
CHARAN TEJA <charan.teja@improve10x.com>
2026-04-06 12:18:49 +0530

    Designed Wellness Products Booking Flow.

--- app/src/main/java/com/iprism/swenhealth/fragments/WellnessProductsFragment.kt
+++ app/src/main/java/com/iprism/swenhealth/fragments/WellnessProductsFragment.kt
@@ -1,13 +1,19 @@
 package com.iprism.swenhealth.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.iprism.swenhealth.R
+import com.iprism.swenhealth.activities.PharmacyProductsActivity
+import com.iprism.swenhealth.adapters.MedicineCategoriesAdapter
 import com.iprism.swenhealth.databinding.FragmentWellnessProductsBinding
 import com.iprism.swenhealth.databinding.ServiceItemBinding
+import com.iprism.swenhealth.interfaces.OnServiceItemClickListener
 
 
 class WellnessProductsFragment : Fragment() {
@@ -19,7 +25,21 @@ class WellnessProductsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentWellnessProductsBinding.inflate(inflater, container, false)
+        setupAdapter()
         return binding.root
+    }
+
+    private fun setupAdapter() {
+        val adapter = MedicineCategoriesAdapter()
+        val linearLayoutManager = GridLayoutManager(requireContext(), 4)
+        binding.categoriesRv.layoutManager = linearLayoutManager
+        binding.categoriesRv.adapter = adapter
+        adapter.setupListener(object : OnServiceItemClickListener{
+            override fun onItemClick(position: Int) {
+                startActivity(Intent(requireContext(), PharmacyProductsActivity::class.java))
+            }
+
+        })
     }
 
 
